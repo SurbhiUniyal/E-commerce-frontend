c542a42e7514187de8b3d7b873132ea2f2460365
SURBHI <128282292+SurbhiUniyal@users.noreply.github.com>
2023-08-28 13:49:19 +0530

    added remove button in cart

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const nav= document.getElementById('navbar');
 const close= document.getElementById("close");
 var btn= document.getElementById("btn");
 
+function deleteRow(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById("myTable").deleteRow(i);
+}
+
 if(bar){
     bar.addEventListener("click", () => {
         nav.classList.add('active');
